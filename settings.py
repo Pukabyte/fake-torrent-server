4645400ba761780a8b1f8d01d43d071ca5685790
Pukabyte <pukabyte@gmail.com>
2025-02-17 13:01:50 +1300

    +

--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     SONARR_URL: str = "http://sonarr:8989"
     RADARR_API_KEY: str = ""
     SONARR_API_KEY: str = ""
-    ZILEAN_URL: str = "http://zilean:8181/torznab/api"
+    ZILEAN_URL: str = "http://zilean:8181"
     MATCH_THRESHOLD: float = 0.8
     LOG_LEVEL: str = "INFO"
     
